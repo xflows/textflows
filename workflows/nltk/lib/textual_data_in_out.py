01697fcdbb3e6f60e8ffc353b3cb208f370f5ec9
Roman <orac.roman@gmail.com>
2014-11-10 21:58:32 +0100

    Load document File/String fixed and concept of a display corpus widget added.

--- workflows/nltk/lib/textual_data_in_out.py
+++ workflows/nltk/lib/textual_data_in_out.py
@@ -4,84 +4,57 @@ import time
 import re
 
 
-def load_adc(inputDict):
+def load_adc_from_file(inputDict):
     file_path = inputDict['file']
     file_name = unicode(os.path.basename(file_path))
+    texts = re.split("[\r\n]", open(file_path, "r").read())
 
     seconds = os.path.getctime(file_path)
     source_date = unicode(time.strftime("%d.%m.%Y %H:%M:%S", time.localtime(seconds)))
     corpus_date = unicode(time.strftime("%d.%m.%Y %H:%M:%S", time.localtime()))
 
-    texts = re.split("[\r\n]", open(file_path, "r").read())
-
-    _tabSeparatedTitle = inputDict['tabSeparatedTitle'] == "true"
-    _leadingLabels = inputDict['leadingLabels'] == "true"
-    documents = []
-    for i, text in enumerate(texts):
-        title = u"Document" + unicode(i + 1)
-        features = {u"contentType": u"Text", u"sourceFileLine": unicode(i)}
-        if _tabSeparatedTitle:
-            text = text.split("\t")
-            title = unicode(text[0])
-            text = u"\t".join(text[1:])
-
-        if _leadingLabels:
-            text = text.split("\t")
-            for feature in [f.strip() for f in text[0].split("!") if f != u""]:
-                features[feature] = "true"
-            text = u"".join(text[1:])
-
-        documents.append(Document(name=title,
-                                  features=features,
-                                  text=text,
-                                  annotations=[Annotation(spanStart=0,
-                                                          spanEnd=max(0, len(unicode(text)) - 1),
-                                                          type1=u"TextBlock",
-                                                          features={})]))
-
+    documents = load_adc(texts, inputDict['tabSeparatedTitle'], inputDict['leadingLabels'])
     features = {u"Source": file_name, u"SourceDate": source_date, u"CorpusCreateDate": corpus_date}
 
     output_dict = {"adc": DocumentCorpus(documents=documents, features=features)}
     return output_dict
 
 
-def load_adcfrom_string(inputDict):
-
+def load_adc_from_string(inputDict):
     source_date = unicode("unknown")
     corpus_date = unicode(time.strftime("%d.%m.%Y %H:%M:%S", time.localtime()))
-
     texts = re.split("[\r\n]", inputDict['plainString'])
 
-    _tabSeparatedTitle = inputDict['tabSeparatedTitle'] == "true"
-    _leadingLabels = inputDict['leadingLabels'] == "true"
-    documents = []
-    for i, text in enumerate(texts):
-        title = u"Document" + unicode(i + 1)
-        features = {u"contentType": u"Text", u"sourceFileLine": unicode(i)}
-        if _tabSeparatedTitle:
-            text = text.split("\t")
-            title = unicode(text[0])
-            text = u"\t".join(text[1:])
-
-        if _leadingLabels:
-            text = text.split("\t")
-            for feature in [f.strip() for f in text[0].split("!") if f != u""]:
-                features[feature] = "true"
-            text = u"".join(text[1:])
-
-        documents.append(Document(name=title,
-                                  features=features,
-                                  text=text,
-                                  annotations=[Annotation(spanStart=0,
-                                                          spanEnd=max(0, len(unicode(text)) - 1),
-                                                          type1=u"TextBlock",
-                                                          features={})]))
-
+    documents = load_adc(texts, inputDict['tabSeparatedTitle'], inputDict['leadingLabels'])
     features = {u"Source": "string", u"SourceDate": source_date, u"CorpusCreateDate": corpus_date}
 
     output_dict = {"adc": DocumentCorpus(documents=documents, features=features)}
     return output_dict
 
+def load_adc(texts,  _tabSeparatedTitle, _leadingLabels):
+    documents = []
+    for i, text in enumerate(texts):
+        title = u"Document" + unicode(i + 1)
+        features = {u"contentType": u"Text", u"sourceFileLine": unicode(i)}
+        if _tabSeparatedTitle == "true":
+            text = text.split("\t")
+            title = unicode(text[0])
+            text = "\t".join(text[1:])
+
+        if _leadingLabels == "true":
+            text = text.split("\t")
+            for feature in [f.strip() for f in text[0].split("!") if f != u""]:
+                features[feature] = "true"
+            text = "".join(text[1:])
+
+        documents.append(Document(name=title,
+                                  features=features,
+                                  text=unicode(text),
+                                  annotations=[Annotation(spanStart=0,
+                                                          spanEnd=max(0, len(unicode(text)) - 1),
+                                                          type1=u"TextBlock",
+                                                          features={})]))
+    return documents
 
 def nltk_show_adc(input_dict):
     if input_dict["adc"] is None:
--- workflows/nltk/urls.py
+++ workflows/nltk/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^get-adc-index/widget(?P<widget_id>[0-9]+)/nx/Index.html$', 'workflows.nltk.views.get_adc_index', name='nltk get adc index'),
     url(r'^get-adc-index/widget(?P<widget_id>[0-9]+)/(?P<narrow_doc>n?)x/Index.html$', 'workflows.nltk.views.get_adc_index', name='nltk get adc index'),
     url(r'^get-adc-index/widget(?P<widget_id>[0-9]+)/(?P<narrow_doc>n?)x/Index(?P<document_id_from>[0-9]+)-(?P<document_id_to>[0-9]+).html$', 'workflows.nltk.views.get_adc_index', name='nltk get adc index'),
+    url(r'^get-adc-index/widget(?P<widget_id>[0-9]+)/(?P<narrow_doc>n?)x/Document(?P<document_id>[0-9]+).html', 'workflows.nltk.views.get_adc_page', name='nltk get adc page'),
 )
