0642ec69b41cd4438fb5bc4420fa5dcc63126d76
Roman <orac.roman@gmail.com>
2014-11-10 15:19:13 +0100

    Code for Load Document Corpus from a file (and String)

--- manage.py
+++ manage.py
@@ -5,7 +5,10 @@
 import os, sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mothra.settings')
+    import clr
+    sys.path.append("C:\\Users\\Roman\\PycharmProjects\\textflows\\workflows\\textflows_dot_net\\bin")
+    import LatinoClowdFlows
 
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mothra.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
