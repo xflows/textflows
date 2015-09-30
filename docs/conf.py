0f8558ab0cace87e04b3819f17a3314994f50724
Darko Aleksovski <darko.aleksovski@ijs.si>
2015-09-30 14:49:32 +0200

    Changed conf.py of Sphinx, for building with RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 import mock, sys
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
